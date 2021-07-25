6a0ca1dee275e6ad330ef79a69db5c67405ff745
Bartek532 <57185551+Bartek532@users.noreply.github.com>
2021-07-25 20:54:52 +0200

    vue: add tests for NewTodoItem component

--- packages/vue/src/__tests__/NewTodoItem.spec.js
+++ packages/vue/src/__tests__/NewTodoItem.spec.js
@@ -1,5 +1,15 @@
-import { screen, render, fireEvent } from "@testing-library/vue";
+import {
+  screen,
+  render,
+  fireEvent,
+  createLocalVue,
+} from "@testing-library/vue";
+import userEvent from "@testing-library/user-event";
 import NewTodoItem from "@/components/NewTodoItem";
+import Vuex from "vuex";
+import Vue from "vue";
+
+Vue.use(Vuex);
 
 test("should properly switch between hide/show states of add todo form", async () => {
   render(NewTodoItem);
@@ -11,4 +21,39 @@ test("should properly switch between hide/show states of add todo form", async (
   expect(form).toHaveClass("active");
   await fireEvent.click(toggleBtn);
   expect(form).not.toHaveClass("active");
+});
+
+describe("adding todo", () => {
+  let actions;
+  let store;
+
+  beforeEach(() => {
+    actions = {
+      addTodo: jest.fn(),
+    };
+    store = new Vuex.Store({
+      actions,
+    });
+  });
+
+  it("should not call addTodo func when form was submitted and input is empty", () => {
+    render(NewTodoItem, { store });
+
+    const submitBtn = screen.getByRole("button", { name: /add/i });
+    userEvent.click(submitBtn);
+
+    expect(actions.addTodo).not.toHaveBeenCalled();
+  });
+
+  it("should call addTodo func with given text when form is submitted and input is not empty", async () => {
+    render(NewTodoItem, { store });
+
+    const submitBtn = screen.getByRole("button", { name: /add/i });
+    const input = screen.getByPlaceholderText(/add a task/i);
+
+    userEvent.type(input, "go to the gym");
+    userEvent.click(submitBtn);
+
+    expect(actions.addTodo).toHaveBeenCalled();
+  });
 });
